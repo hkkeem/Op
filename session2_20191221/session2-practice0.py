9fbc5cd4300450926e6feed46a8a8d422ebb4f52
hkkeem <58875725+hkkeem@users.noreply.github.com>
2020-01-11 13:25:03 +0900

    오타수정

--- session2_20191221/session2-practice0.py
+++ session2_20191221/session2-practice0.py
@@ -2,7 +2,9 @@ menu = {'라면' : 4000, '만두' : 3500, '보쌈' : 20000}
 menuName = input('메뉴를 입력하세요 : ')
 
 if menuName in menu:
-    cost = menu[manuName]
+    cost = menu[menuName]
     print(menuName + "의 가격은" + str(cost) + "원 입니다.")
 else:
     print(menuName + "란 메뉴는 없습니다.")
+
+    
